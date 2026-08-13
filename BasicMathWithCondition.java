be0f2d0fb62d5ed6be384a4ec566b48ae87fec52
hans <banayathansvalentin_bsit@plmun.edu.ph>
2026-08-13 11:06:31 +0800

    First Java program upload

---
+++ BasicMathWithCondition.java
@@ -0,0 +1,18 @@
+public class SimpleProgram {
+    public static void main(String[] args) {
+        int number1 = 10;
+        int number2 = 5;
+        int sum = number1 + number2;
+
+        System.out.println("The sum is: " + sum);
+
+        if (sum > 10) {
+            System.out.println("The sum is greater than 10.");
+        } else {
+            System.out.println("The sum is not greater than 10.");
+        }
+
+        int difference = number1 - number2;
+        System.out.println("The difference is: " + difference);
+    }
+}
